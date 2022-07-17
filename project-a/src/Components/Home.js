52ccf958d6ce6c9e55924e0ab7033c7a47dd8757
Nung Khual <nung.khual@labs196.com>
2022-07-17 01:27:32 -0500

    Nav

---
+++ project-a/src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Nav from './NavBar/Nav'
+
+function Home() {
+  return (
+    <Nav />
+  )
+}
+
+export default Home
---
+++ project-a/src/Components/NavBar/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../Styles/Nav.css";
+
+function Nav() {
+  return (
+    <Row className="Nav-Wrapper">
+      <Col> </Col>
+    </Row>
+  );
+}
+
+export default Nav;
